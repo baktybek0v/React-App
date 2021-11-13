94a1d148412baaf24bf97e4e1c3d772552120e95
baktybek0v <baktybekov.ernis@gmail.com>
2021-11-13 10:48:06 +0600

    change Browser Router to Hash Router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 //import React, { Fragment } from 'react';
 import React from 'react';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import {HashRouter, Route, Switch} from 'react-router-dom';
+
+//import { BrowserRouter } from "react-router-dom"
+
 
 import Home from './pages/Home.js';
 import Model1 from'./pages/Model1.js';
@@ -8,13 +11,13 @@ import Model2 from'./pages/Model2.js';
 
 function App() {
     return (
-        <BrowserRouter basename="process.env.PUBLIC_URL">
+        <HashRouter basename="process.env.PUBLIC_URL">
                 <Switch>
                     <Route path={'/'} exact component={Home}/>
                     <Route path={'/Model1'} component={Model1}/>
                     <Route path={'/Model2'} component={Model2}/>
                 </Switch>
-        </BrowserRouter>
+        </HashRouter>
     );
 }
 
